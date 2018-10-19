7ff89505e52a8837f7af9fc45e3ef87eba66040f
Riba0017 <Riba0017@yandex.ru>
2018-10-19 19:32:30 +0300

    add jquery

--- misocial/js/index.js
+++ misocial/js/index.js
@@ -1,17 +1,8 @@
-/**
- * Created by User on 22.09.2018.
- */
 
-var toggle = document.querySelector(".menu-link");
-var mainNav = document.querySelector(".main-nav");
-var mainList = document.querySelector(".menu-list");
-
-
-toggle.addEventListener("click", function (event) {
-    event.preventDefault();
-    mainNav.classList.remove("main-nav");
-    mainNav.classList.add("mob-nav");
-    mainNav.classList.add("open-nav");
-    mainList.classList.remove("menu-list");
-    mainList.classList.add("mob-list");
+$(document).ready(() => {
+    $('.menu-link').on('click', () => {
+        $('.mob-nav').addClass('open-nav');
+    }).on('swiperight', () => {
+        $('.mob-nav').removeClass('open-nav');
+    });
 });
