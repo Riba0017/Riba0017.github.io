a6598422b9738a0da80e9b25634f8859ec176d5a
Kate <Riba0017@yandex.ru>
2019-08-08 16:50:34 +0300

    Update push-modify.js

--- push-modify.js
+++ push-modify.js
@@ -1,26 +1,44 @@
-function notifyMe() {
-  // Let's check if the browser supports notifications
-  if (!("Notification" in window)) {
-    console.log("This browser does not support desktop notification");
-  }
 
-  // Let's check whether notification permissions have alredy been granted
-  else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    var notification = new Notification("Hi there!");
-  }
+function initPushNotificationsPopupAsker() {
+    var $cancelButton = $('.js-ask-later-button');
+    var $popup = $('.js-popup');
+    var $overlay = $('#overlay');
 
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== 'denied' || Notification.permission === "default") {
-    Notification.requestPermission(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        var notification = new Notification("Hi there!");
-      }
+    $cancelButton.on('click', function() {
+        $overlay.fadeOut();
+        $popup.fadeOut();
     });
-  }
 
-  // At last, if the user has denied notifications, and you 
-  // want to be respectful there is no need to bother them any more.
+    // Let's check if the browser supports notifications
+    if ("Notification" in window) {
+        notifyMe();
+    } else {
+        console.log("This browser does not support desktop notification");
+    }
 }
-console.log(notifyMe());
+
+function notifyMe() {
+    var $popup = $('.js-popup');
+    var $overlay = $('#overlay');
+
+    // Let's check whether notification permissions have already been granted
+    if (Notification.permission === "granted") {
+        console.log('User is already subscribed');
+    }
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== 'denied' || Notification.permission === "default") {
+        $overlay.delay(2000).fadeIn();
+        $popup.delay(2000).fadeIn();
+        Notification.requestPermission(function (permission) {
+            // If the user accepts, let's create a notification
+            if (permission === "granted") {
+                var notification = new Notification("Hi there!");
+            }
+        });
+    }
+
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them any more.
+}
+
+initPushNotificationsPopupAsker();
