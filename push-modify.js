cffae8b71213775e3b96181f69ecc7b27adc035f
Kate <Riba0017@yandex.ru>
2019-08-30 14:14:30 +0300

    Update push-modify.js

--- push-modify.js
+++ push-modify.js
@@ -79,28 +79,28 @@ $(document).ready(function() {
 
 //demo
 
-function addEventToGTA() {
-    var demoButton = $('.js-demo-btn');
+// function addEventToGTA() {
+//     var demoButton = $('.js-demo-btn');
     // demo request button
 //     demoButton.on('click', function() {
 //         if (dataLayer && typeof dataLayer.push === 'function') {
 //             dataLayer.push({'event':'request-demo'});
 //         }
 //     });
-    demoButton.bind('click', function() {
-        $.ajax({
-            url: 'http://192.168.88.124/support/contact_us/index.php',
-            type: 'POST',
-            data: ({ request: 'demo' }),
-            dataType: 'html',
-            success: funcSuccess
-        })
-    })
-}
+//     demoButton.bind('click', function() {
+//         $.ajax({
+//             url: 'http://192.168.88.124/support/contact_us/index.php',
+//             type: 'POST',
+//             data: ({ request: 'demo' }),
+//             dataType: 'html',
+//             success: funcSuccess
+//         })
+//     })
+// }
 
-function funcSuccess(data) {
-    return console.log(data);
-}
+// function funcSuccess(data) {
+//     return console.log(data);
+// }
 
 $(document).ready(function() {
         var demoButton = $('.js-demo-btn');
